3424b59f9f9cc2f0122b03bc94ec18738869aa86
LuanFlorencio <luanflorencio.dev@gmail.com>
2023-10-19 13:03:40 -0300

    fix: adjust style Button component

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, icon, width, ...props }: ButtonProps)
     <button
       {...props}
       data-width={width}
-      className="w-full data-[width=just-content]:w-max h-12 px-4 rounded-lg bg-brand md:hover:brightness-75 transition flex items-center gap-2 font-sans font-bold text-sm text-neutral-300"
+      className="w-full data-[width=just-content]:w-max h-12 px-4 rounded-lg bg-brand md:hover:brightness-75 transition flex items-center justify-center gap-2 font-sans font-bold text-sm text-neutral-300"
     >
       {icon} {children}
     </button>
